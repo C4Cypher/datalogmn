249c8457062266358a32f64a32424a2874663979
C4Cypher <c4cypher@gmail.com>
2023-06-08 13:57:06 -0400

    Better semantics

--- datalog.m
+++ datalog.m
@@ -31,24 +31,57 @@
 :- type relation --->
 	relation( name :: string, arity :: uint ).
 	
-:- pred relation(literal(T):in, relation:out) is det.
-:- func relation(literal(T)) = relation.
+:- pred relation(atom(T):in, relation:out) is det.
+:- func relation(atom(T)) = relation.
 
-:- type literal(T). 
-:- type literal == literal(generic).
+% An atom is a combination of a function symbol and a list of terms.
+% In implementation, the string symbol will be interned
+:- type atom(T). 
+:- type atom == atom(generic).
 
+:- pred atom(string, list(term(T)), atom(T)).
+:- mode atom(in, in, out) is det.
+:- mode atom(out,out, in) is det.
 
+:- func atom(string, list(term(T))) = atom(T).
+:- mode atom(in, in) = out is det.
+:- mode atom(out, out) = in is det.
+
+:- pred unify_atoms(atom(T):in, atom(T)in, substitution(T):in, 
+	substitution(T):out) is det.
+	
+% A literal is an atom or it's negation.
+:- type literal(T) ---> positive(atom(T)) ; negative(atom(T)).
+
+% Syntactic constructor for positive literals. Fails if literal is negated.
+% To construct a negative literal use not literal(X, Y) or negation/2
 :- pred literal(string, list(term(T)), literal(T)).
 :- mode literal(in, in, out) is det.
-:- mode literal(out,out, in) is det.
+:- mode literal(out, out, in) is semidet. 
 
-:- func literal(string, list(term(T))) = literal(T).
+:- func literal(string, list(term(T)) = literal(T).
 :- mode literal(in, in) = out is det.
-:- mode literal(out, out) = in is det.
+:- mode literal(out, out) = in is semidet.
+
+% negation(X, Y) :- X = not Y.
+:- pred negation(literal(T), literal(T)).
+:- mode negation(in, out) is det.
+:- mode negation(out, in) is det.
+
+:- func not literal(T) = literal(T).
+:- mode not in = out is det.
+:- mode not out = in is det.
+
+% True if there are no variables in atom
+:- pred is_ground(atom(T):in) is semidet.
+
+:- pred vars(atom(T):in, list(var(T)):out) is det.
+:- func vars(atom(T)) = list(var(T)).
+
+:- type rule(T)
 
 
-
-
+% pred types that can be passed as primitive rules
 :- type primitive == pred(list(term(T)):in, list(term(T)):out) is nondet.
 
 % If the primitive succeeds, return the input.
