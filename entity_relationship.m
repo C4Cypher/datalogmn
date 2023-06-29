1003a6ef5a711272a26a968b18d9747dcb132db9
C4Cypher <c4cypher@gmail.com>
2023-06-29 10:43:07 -0400

    Update entity_relationship.m

--- entity_relationship.m
+++ entity_relationship.m
@@ -17,11 +17,52 @@
 :- import_module term.
 
 
-:- type er_term	--->
+:- type er_term(T)	--->
 	entity(uint) ;
-	variable(var(er_term)) ;
+	variable(var(T)) ;
 	symbol(symbol) ;
 	name(string) ;
+	color(string) ;
 	stat(int) ;
 	scalar(float) ;
-	rel
+	assertion(symbol, entity_term, object_term) ;
+	bool_assertion(symbol, entity_term) ;
+	not(assertion_term) ;
+	and(list(assertion_term)) ;
+	or(list(assertion_term)).
+	
+:- type object_term(T) =< er_term(T) --->
+	entity(uint) ;
+	variable(var(T)) ;
+	name(string) ;
+	color(string) ;
+	stat(int) ;
+	scalar(float).
+	
+:- type entity_term(T) =< object_term(T) --->
+	entity(uint) ;
+	variable(var(T)).
+	
+:- type assertion_term(T) =< er_term	--->
+	variable(var(T)) ;
+	assertion(symbol, entity_term, object_term) ;
+	bool_assertion(symbol, entity_term) ;
+	not(assertion_term) ;
+	and(list(assertion_term)) ;
+	or(list(assertion_term)).
+	
+:- type positive_assertion_term(T) =< assertion_term	--->
+	variable(var(T)) ;
+	assertion(symbol, entity_term, object_term) ;
+	bool_assertion(symbol, entity_term) ;
+	and(list(positive_assertion_term)) ;
+	or(list(positive_assertion_term)).
+
+:- type negated_assertion_term(T) =< assertion_term --->
+	not(assertion_term).
+	
+
+	
+	
+	
+	
